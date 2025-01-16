16b02dd741acea18160fcca67e07bdf53c8b3e31
JRejas <rejas.jose@gmail.com>
2025-01-16 01:06:35 -0300

    vistas hbs pt1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,7 +7,7 @@ import {
   cancelarAdoption,
 } from "../controllers/petController.js";
 import middlewareJWT from "../config/auth.middleware.js";
-import { adminPetController } from "../controllers/admin.controller.js";
+import { adminPetController, adminPetControllerAgregarForm, adminPetControllerAgregar, adminPetControllerEdit } from "../controllers/admin.controller.js";
 
 const routes = (app) => {
   const router = Router();
@@ -23,7 +23,11 @@ const routes = (app) => {
   // admin
 
   router.get("/admin/mascotas", middlewareJWT, adminPetController);
-  router.get("/admin/mascotas/editar/:id");
+  router.post("/admin/mascotas", middlewareJWT, adminPetController);
+  router.get("/admin/agregarPet", middlewareJWT, adminPetControllerAgregarForm);
+  router.post("/admin/agregarPet", middlewareJWT, adminPetControllerAgregar);
+  router.get("/admin/editar/:id",  middlewareJWT, adminPetControllerEdit);
+  
 
   // auth
 
