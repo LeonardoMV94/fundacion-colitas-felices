aac3b6d44194096b639cc61f18e76a99c73e19e4
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2025-01-24 00:30:45 -0300

    middleware jwt & roles

--- src/config/auth.middleware.js
+++ src/config/auth.middleware.js
@@ -1,12 +1,54 @@
-const isHaveToken = (req, res, next) => {
-  next();
+import jwt from 'jsonwebtoken';
+import env from '../../env.js';
+import User from '../models/user.model.js';
+
+const isHaveToken = async(req, res, next) => {
+  try {
+    const token = req.cookies._token;
+    console.log('token de cookie: ', token);
+    if (!token) {
+      return res.redirect('/auth/login');
+    }    
+
+    const { id } = jwt.verify(token, env.jwtSecret);
+
+    const usuario = await User.findByPk(id);
+    if( !usuario ) {
+      return res.redirect('/auth/login');
+    }
+    
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.redirect('/auth/login');
+  }
 };
 
 const isAdminRol = (req, res, next) => {
-  next();
+  try {
+    const token = req.cookies._token;
+    console.log('token de cookie: ', token);
+    if (!token) {
+      return res.redirect('/auth/login');
+    }
+
+    const { rol } = jwt.verify(token, env.jwtSecret);
+    console.log('rol: ', rol);
+
+    if(rol !== 'administrador') {
+      return res.render('auth/noEresAdmin', {
+        layout: 'sin-partials'
+      });
+    }
+    
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.render('auth/login', {
+      layout: 'sin-partials',
+      errores: [{ msg: `error en el servidor` }]
+    });
+  }
 };
 
-export {
-  isHaveToken,
-  isAdminRol
-}
+export { isHaveToken, isAdminRol };
